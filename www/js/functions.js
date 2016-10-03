17ee46aaf78105d838f02284880e3d42ee028f0b
Ivan Klykov <nikolateslakem@gmail.com>
2016-10-03 15:41:53 +0300

    our_blog

--- www/js/functions.js
+++ www/js/functions.js
@@ -20,3 +20,17 @@ $(document).ready(function(){
 				};
 			});
 		});
+
+
+//example
+$(document).ready(function(){
+ 
+  // hide our element on page load
+
+ $('#element-to-animate').css('opacity', 0);
+
+  $('#element-to-animate').waypoint(function() {
+      $('#element-to-animate').addClass('fadeInLeft');
+  }, { offset: '100%' });
+ 
+});
